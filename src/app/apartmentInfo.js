529a9e5d382c5fa505ff76c4e16f4f6d72872f78
ikape <ikapeisaac3@gmail.com>
2025-08-22 17:06:20 +0100

    remove address from ApartmentInfo component display

--- src/app/apartmentInfo.js
+++ src/app/apartmentInfo.js
@@ -125,7 +125,7 @@ export default function ApartmentInfo() {
         <div className="flex gap-[3px] items-center">
           <FaMapMarkerAlt className="text-gray-400 mr-2 text-xl" />
           <span className="text-gray-600">
-            {apartment.address}, {apartment.city}, {apartment.state}
+          {apartment.city}, {apartment.state}
           </span>
         </div>
 
